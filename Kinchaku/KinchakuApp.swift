cbace8f83fe9e69bfaf8c81efd583298c88dff86
Eric Masiello <emasiello@vistaprint.com>
2025-09-04 20:31:19 -0400

    Initial Commit

---
+++ Kinchaku/KinchakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinchakuApp.swift
+//  Kinchaku
+//
+//  Created by Eric Masiello on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct KinchakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KinchakuTests/KinchakuTests.swift
@@ -0,0 +1,16 @@
+//
+//  KinchakuTests.swift
+//  KinchakuTests
+//
+//  Created by Eric Masiello on 9/4/25.
+//
+
+import Testing
+
+struct KinchakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
