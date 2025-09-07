db6dfbc533371fcd9a82643047cbe5038a9c79b2
Eric Masiello <emasiello@vistaprint.com>
2025-09-07 08:42:42 -0400

    chore: add configurable path for testing

--- Kinchaku/Networking/BaseAPIService.swift
+++ Kinchaku/Networking/BaseAPIService.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 enum BaseAPIService {
-  static let url = "https://kinchaku.synology.me/api/v1"
+    static let url = "https://kinchaku.synology.me/api/v1"
+    // static let url = "http://localhost:3000/api/v1"
 }
